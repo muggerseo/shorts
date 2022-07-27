6b1a919def9ec20c62214499f63da23fd065ea0d
muggerseo <65226654+muggerseo@users.noreply.github.com>
2022-07-27 13:18:12 +0200

    Add files via upload

---
+++ random password.py
@@ -0,0 +1,13 @@
+import random
+
+lower_case = "abcdefghigklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+symbols = "@#$%&*/\?"
+
+Use_for = lower_case + upper_case + number + symbols
+length_for_pass = 16
+
+password = "".join(random.sample(Use_for, length_for_pass))
+    
+print("Your generated password is: " + password)
