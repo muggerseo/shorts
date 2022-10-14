d9f2be3f33d2bc70b29519fe885265bd0a9d41e3
muggerseo <65226654+muggerseo@users.noreply.github.com>
2022-10-14 18:50:44 +0200

    Bubble sort of numbers with for loop

---
+++ bubble sort.py
@@ -0,0 +1,11 @@
+n = 6
+mas = [5,7,4,3,8,2]
+count = 0
+
+for run in range(n-1):
+    for i in range(n-1):
+        if mas[i] > mas[i+1]:
+            count += 1
+            mas[i],mas[i+1] = mas[i+1],mas[i]
+
+print(mas)
