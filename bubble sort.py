c9f346b667718e4049ab88bc5c5f4bdac94000c6
muggerseo <65226654+muggerseo@users.noreply.github.com>
2022-10-14 19:23:24 +0200

    Update bubble sort.py

--- bubble sort.py
+++ bubble sort.py
@@ -1,14 +1,13 @@
-n = 6
-mas = [5,7,4,3,8,2]
+n = int(input())
+mas = list(map(int,input().split()))
 count = 0
 
 for run in range(n-1):
-    for i in range(n-1):
+    for i in range(n-1-run):
+        # print(f'now compire {mas[i]} and {mas[i+1]}')
         if mas[i] > mas[i+1]:
             count += 1
             mas[i],mas[i+1] = mas[i+1],mas[i]
-for i in mas:
-    print(i, end=' ')
 
-print()
+print(*mas)
 print(count)
